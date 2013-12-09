38f7d89e2915a70e5e8f6d37b024139e5a4f9880
NBarnes <nbarnes@gmail.com>
2013-12-08 21:48:35 -0800

    Partially moved into the testing world.  Core application functionality intact.

---
+++ spec/decoderSpec.js
@@ -0,0 +1,10 @@
+describe("Decoder", function() {
+  it("decodes an encrypted string from the URL", function() {
+    // override the getCypherText and getPassword functions because
+    // we can't access document.URL directly
+    // and we can't ask the user for interaction
+    decoder.getCypherText = function() { decoder.cypherText = CryptoJS.AES.encrypt("bob keeps it cool","password"); };
+    decoder.getPassword   = function() { decoder.password = "password"; };
+    expect(decoder.decrypt()).toEqual("a test string");
+  });
+});
---
+++ src/decoder.js
@@ -0,0 +1,14 @@
+
+var decoder = {
+  getPassword: function() {
+    this.password = prompt("Enter password to decode the message")
+  },
+  decryptMessage: function() {
+    this.message = document.URL.split('?')[1]
+  },
+  decrypt: function() {
+    this.getPassword()
+    this.decryptMessage()
+    alert(CryptoJS.AES.decrypt(this.message, this.password).toString(CryptoJS.enc.Utf8))
+  }
+}
